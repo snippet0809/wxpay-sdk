2ca1f3b3590c75938aaca101a985b7db5cd2ab99
Spet <snippet0809@163.com>
2021-04-27 09:03:29 +0800

    ~ v1.0.1

--- src/main/java/io/github/snippet0809/wxpay_sdk/security/AesDecoder.java
+++ src/main/java/io/github/snippet0809/wxpay_sdk/security/AesDecoder.java
@@ -13,13 +13,13 @@ import java.util.Base64;
 
 public class AesDecoder {
 
-    private final String aesKey;
+    private final byte[] aesKey;
 
     private static final int KEY_LENGTH_BYTE = 32;
     private static final int TAG_LENGTH_BIT = 128;
 
-    public AesDecoder(String aesKey) {
-        if (aesKey.length() != KEY_LENGTH_BYTE) {
+    public AesDecoder(byte[] aesKey) {
+        if (aesKey.length != KEY_LENGTH_BYTE) {
             throw new IllegalArgumentException("无效的ApiV3Key，长度必须为32个字节");
         }
         this.aesKey = aesKey;
@@ -29,7 +29,7 @@ public class AesDecoder {
         try {
             Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
 
-            SecretKeySpec key = new SecretKeySpec(aesKey.getBytes(StandardCharsets.UTF_8), "AES");
+            SecretKeySpec key = new SecretKeySpec(aesKey, "AES");
             GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH_BIT, nonce.getBytes(StandardCharsets.UTF_8));
 
             cipher.init(Cipher.DECRYPT_MODE, key, spec);
