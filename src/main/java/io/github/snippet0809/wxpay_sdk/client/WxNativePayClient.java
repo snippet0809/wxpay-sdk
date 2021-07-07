753ed6ab452aff3c7a0b16b7f8b3f3db37b3f844
Spet <snippet0809@163.com>
2021-07-07 14:16:47 +0800

    ~ v1.2.0 支持native支付

---
+++ src/main/java/io/github/snippet0809/wxpay_sdk/client/WxNativePayClient.java
@@ -0,0 +1,46 @@
+package io.github.snippet0809.wxpay_sdk.client;
+
+import com.alibaba.fastjson.JSONObject;
+import io.github.snippet0809.wxpay_sdk.constant.WxApi;
+import io.github.snippet0809.wxpay_sdk.exception.WxPayApiException;
+import io.github.snippet0809.wxpay_sdk.pojo.Amount;
+import io.github.snippet0809.wxpay_sdk.pojo.OrderBody;
+import io.github.snippet0809.wxpay_sdk.pojo.OrderResult;
+import io.github.snippet0809.wxpay_sdk.pojo.WakeAppPayParam;
+import io.github.snippet0809.wxpay_sdk.security.SignProducer;
+import io.github.snippet0809.wxpay_sdk.security.SignVerifier;
+
+import java.io.IOException;
+
+public class WxNativePayClient extends WxPayClient {
+
+    private final String appid;
+
+    public WxNativePayClient(String appid, SignProducer signProducer, SignVerifier signVerifier) {
+        super(signProducer, signVerifier);
+        this.appid = appid;
+    }
+
+    /**
+     * App支付下单
+     */
+    public String ordering(OrderBody orderBody) throws IOException, WxPayApiException {
+        return wxPayHttpClient.post(WxApi.TRANSACTION_NATIVE, JSONObject.toJSONString(orderBody));
+    }
+
+    /**
+     * 最简单的App支付下单
+     */
+    public String simplestOrdering(String outTradeNo, String goodsDesc, int total, String notifyUrl) throws WxPayApiException, IOException {
+        OrderBody orderBody = new OrderBody();
+        orderBody.setAppid(appid);
+        orderBody.setMchId(signProducer.getMchId());
+        orderBody.setDescription(goodsDesc);
+        orderBody.setOutTradeNo(outTradeNo);
+        orderBody.setNotifyUrl(notifyUrl);
+        Amount amount = new Amount();
+        amount.setTotal(total);
+        orderBody.setAmount(amount);
+        return ordering(orderBody);
+    }
+}
--- src/main/java/io/github/snippet0809/wxpay_sdk/constant/WxApi.java
+++ src/main/java/io/github/snippet0809/wxpay_sdk/constant/WxApi.java
@@ -5,6 +5,7 @@ public interface WxApi {
     String TRANSACTION_APP = "https://api.mch.weixin.qq.com/v3/pay/transactions/app";
     String TRANSACTION_JSAPI = "https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi";
     String TRANSACTION_H5 = "https://api.mch.weixin.qq.com/v3/pay/transactions/h5";
+    String TRANSACTION_NATIVE = "https://api.mch.weixin.qq.com/v3/pay/transactions/native";
 
     String REFUND = "https://api.mch.weixin.qq.com/v3/refund/domestic/refunds";
 
